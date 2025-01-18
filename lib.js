18c456dfb4213e76ef3b05042794304ae0715f34
soroushGH <soroushdeveloper20@gmail.com>
2025-01-18 09:17:50 +0300

    Added the layers for nested data.

--- lib.js
+++ lib.js
@@ -10,11 +10,50 @@ export class YoozORM {
 
     if (!where || !where.category) return "group cannot be empty.";
 
+    function formatData(data) {
+      const entries = Object.entries(data).map(([key, value]) => {
+        if (typeof value === "object" && value !== null) {
+  
+          const nestedEntries = Object.entries(value).map(
+            ([nestedKey, nestedValue]) => {
+              return `-${nestedKey}, +${nestedValue}`;
+            }
+          );
+          return `${key}=[${nestedEntries.join(", ")}]`;
+        }
+        return `${key}=${value}`;
+      });
+      return entries.join(", ");
+    }
+  
+    function formatCondition(data) {
+      
+      const entries = Object.entries(data).map(([key, value]) => {
+        if (typeof value === "object" && value !== null) {
+  
+          const nestedEntries = Object.entries(value).map(
+            ([nestedKey, nestedValue]) => {
+              return `-${nestedKey}, +${nestedValue}`;
+            }
+          );
+          return `${key}==[${nestedEntries.join(", ")}]`;
+        }
+        if(!where.layer || where.layer === 0){
+          return `${key}==${value}`;
+        }
+        return `${key}(${where.layer})==${value}`;
+      });
+      return entries.join(", ");
+    }
+
     switch (action.action) {
       case "delete":
-        const formattedString = this.formatData(data);
-        query = `REMOVE FROM ${where.category}("${formattedString}") IF ${this.formatCondition(where.condition)}`;
+
+        const formattedString = formatData(data);
+
+        query = `REMOVE FROM ${where.category}("${formattedString}") IF ${formatCondition(where.condition)}`;
         return query;
+        
 
       case "create":
         break;
@@ -27,35 +66,5 @@ export class YoozORM {
     }
   }
 
-  formatData(data) {
-    const entries = Object.entries(data).map(([key, value]) => {
-      if (typeof value === "object" && value !== null) {
-
-        const nestedEntries = Object.entries(value).map(
-          ([nestedKey, nestedValue]) => {
-            return `-${nestedKey}, +${nestedValue}`;
-          }
-        );
-        return `${key}=[${nestedEntries.join(", ")}]`;
-      }
-      return `${key}=${value}`;
-    });
-    return entries.join(", ");
-  }
-
-  formatCondition(data) {
-    const entries = Object.entries(data).map(([key, value]) => {
-      if (typeof value === "object" && value !== null) {
-
-        const nestedEntries = Object.entries(value).map(
-          ([nestedKey, nestedValue]) => {
-            return `-${nestedKey}, +${nestedValue}`;
-          }
-        );
-        return `${key}==[${nestedEntries.join(", ")}]`;
-      }
-      return `${key}==${value}`;
-    });
-    return entries.join(", ");
-  }
+  
 }
